156d0060e24ed2681f8d72f71dc7747e570d2adf
ngoc cuong nguyen <cnguy049@meow.cs.fiu.edu>
2022-10-27 14:54:45 -0400

    add linear layers and gaussian distribution

---
+++ bnn/models/gaussian.py
@@ -0,0 +1,37 @@
+import math
+import torch
+
+class Gaussian(object):
+    def __init__(self, mu, rho, device='cpu'):
+        super().__init__()
+        self.mu = mu
+        self.rho = rho
+        self.normal = torch.distributions.Normal(0,1)
+        self.device = device
+    
+    @property
+    def sigma(self):
+        return torch.log1p(torch.exp(self.rho))
+    
+    def sample(self):
+        epsilon = self.normal.sample(self.rho.size()).to(self.device)
+        return self.mu + self.sigma * epsilon
+    
+    def log_prob(self, input):
+        return (-math.log(math.sqrt(2 * math.pi))
+                - torch.log(self.sigma)
+                - ((input - self.mu) ** 2) / (2 * self.sigma ** 2)).sum()
+    
+class ScaleMixtureGaussian(object):
+    def __init__(self, pi, sigma1, sigma2):
+        super().__init__()
+        self.pi = pi
+        self.sigma1 = sigma1
+        self.sigma2 = sigma2
+        self.gaussian1 = torch.distributions.Normal(0,sigma1)
+        self.gaussian2 = torch.distributions.Normal(0,sigma2)
+    
+    def log_prob(self, input):
+        prob1 = torch.exp(self.gaussian1.log_prob(input))
+        prob2 = torch.exp(self.gaussian2.log_prob(input))
+        return (torch.log(self.pi * prob1 + (1-self.pi) * prob2)).sum()
--- bnn/models/layers/linear.py
+++ bnn/models/layers/linear.py
@@ -1,16 +1,66 @@
-from ..module import BNNModule
-from torch import Tensor
+# from ..module import BNNModule
+# from torch import Tensor
 
 
-class Linear(BNNModule):
-    def __init__(self, **kwargs):
+# class Linear(BNNModule):
+#     def __init__(self, **kwargs):
+#         super().__init__()
+
+#     def _kl(self) -> Tensor:
+#         pass
+
+#     def _sample_weight(self) -> Tensor:
+#         pass
+
+#     def forward(self, input: Tensor) -> Tensor:
+#         pass
+import torch
+import torch.nn as nn
+import torch.functional as F
+
+from ..gaussian import Gaussian, ScaleMixtureGaussian
+
+
+class BayesianLinear(nn.Module):
+    def __init__(self, in_features, out_features, pi, sigma_1, sigma_2, device = 'cpu'):
         super().__init__()
 
-    def _kl(self) -> Tensor:
-        pass
+        self.in_features = in_features
+        self.out_features = out_features
 
-    def _sample_weight(self) -> Tensor:
-        pass
+        # Weight parameters
+        self.weight_mu = nn.Parameter(torch.Tensor(out_features, in_features).uniform_(-0.2, 0.2)) # do we need to modify range here
+        self.weight_rho = nn.Parameter(torch.Tensor(out_features, in_features).uniform_(-5,-4)) # de we need to modify range here
+        self.weight = Gaussian(self.weight_mu, self.weight_rho, device)
+        
+        # Bias parameters
+        self.bias_mu = nn.Parameter(torch.Tensor(out_features).uniform_(-0.2, 0.2))
+        self.bias_rho = nn.Parameter(torch.Tensor(out_features).uniform_(-5,-4))
+        self.bias = Gaussian(self.bias_mu, self.bias_rho, device)
+        # Prior distributions
 
-    def forward(self, input: Tensor) -> Tensor:
-        pass
+        self.pi = pi
+        self.sigma_2 = sigma_1
+        self.sigma_2 = sigma_2 
+
+        self.device = device
+
+        self.weight_prior = ScaleMixtureGaussian(pi, sigma_1, sigma_2)
+        self.bias_prior = ScaleMixtureGaussian(pi, sigma_1, sigma_2)
+        self.log_prior = 0
+        self.log_variational_posterior = 0
+
+    def forward(self, input, sample=False, calculate_log_probs=False):
+        if self.training or sample:
+            weight = self.weight.sample()
+            bias = self.bias.sample()
+        else:
+            weight = self.weight.mu
+            bias = self.bias.mu
+        if self.training or calculate_log_probs:
+            self.log_prior = self.weight_prior.log_prob(weight) + self.bias_prior.log_prob(bias)
+            self.log_variational_posterior = self.weight.log_prob(weight) + self.bias.log_prob(bias)
+        else:
+            self.log_prior, self.log_variational_posterior = 0, 0
+
+        return F.linear(input, weight, bias)
