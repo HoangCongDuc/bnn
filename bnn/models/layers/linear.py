9edb81f004f681a1fbb1cd23ee3ae35e2118b037
HoangCongDuc <dhoan005@fiu.edu>
2022-11-15 21:13:06 -0500

    Add mixture model

--- bnn/models/layers/linear.py
+++ bnn/models/layers/linear.py
@@ -14,23 +14,29 @@ __all__ = ['Linear']
 
 class Linear(BNNModule):
     def __init__(self, in_features: int, out_features: int, bias: bool = True,
-                 device=None, dtype=torch.float32) -> None:
+                 device=None, dtype=torch.float32,
+                 logstd=(0,), mixture_weights=(1,)) -> None:
+        assert len(logstd) == len(mixture_weights)
         factory_kwargs = {'device': device, 'dtype': dtype}
         super().__init__()
         self.in_features = in_features
         self.out_features = out_features
 
-        weight_prior_mean = torch.zeros((out_features, in_features), **factory_kwargs)
-        weight_prior_std = torch.ones((out_features, in_features), **factory_kwargs)
+        mixture_len = len(mixture_weights)
+        std = torch.exp(torch.tensor(logstd, **factory_kwargs))
+        weight_prior_mean = torch.zeros((out_features, in_features, mixture_len), **factory_kwargs) * std
+        weight_prior_std = torch.ones((out_features, in_features, mixture_len), **factory_kwargs) * std
+        mixture_weights = torch.tensor(mixture_weights, **factory_kwargs)
         self.register_buffer('weight_prior_mean', weight_prior_mean, False)
         self.register_buffer('weight_prior_std', weight_prior_std, False)
+        self.register_buffer('mixture_weights', mixture_weights, False)
 
         self.weight_posteiror_mean = nn.Parameter(torch.empty((out_features, in_features), **factory_kwargs))
         self.weight_posteiror_rho = nn.Parameter(torch.empty((out_features, in_features), **factory_kwargs))
 
         if bias:
-            bias_prior_mean = torch.zeros(out_features, **factory_kwargs)
-            bias_prior_std = torch.ones(out_features, **factory_kwargs)
+            bias_prior_mean = torch.zeros((out_features, mixture_len), **factory_kwargs) * std
+            bias_prior_std = torch.ones((out_features, mixture_len), **factory_kwargs) * std
             self.register_buffer('bias_prior_mean', bias_prior_mean, False)
             self.register_buffer('bias_prior_std', bias_prior_std, False)
 
@@ -63,13 +69,51 @@ class Linear(BNNModule):
         
         return result
 
-    def _kl(self) -> Tensor:
-        posterior = self._get_posterior()
-        weight_prior = D.Independent(D.Normal(self.weight_prior_mean, self.weight_prior_std), 2)
-        kl = D.kl_divergence(posterior['weight'], weight_prior)
+    def _get_prior(self) -> Dict[str, D.Distribution]:
+        result = dict()
+
+        if len(self.mixture_weights) == 1:
+            weight_prior = D.Normal(self.weight_prior_mean.squeeze(-1), self.weight_prior_std.squeeze(-1))
+        else:
+            weight_prior_mix = D.Categorical(self.mixture_weights.expand(self.out_features, self.in_features, -1))
+            weight_prior_comp = D.Normal(self.weight_prior_mean, self.weight_prior_std)
+            weight_prior = D.MixtureSameFamily(weight_prior_mix, weight_prior_comp)
+        result['weight'] = D.Independent(weight_prior, 2)
+
         if self.use_bias:
-            bias_prior = D.Independent(D.Normal(self.bias_prior_mean, self.bias_prior_std), 1)
-            kl = kl + D.kl_divergence(posterior['bias'], bias_prior)
+            if len(self.mixture_weights) == 1:
+                bias_prior = D.Normal(self.bias_prior_mean.squeeze(-1), self.bias_prior_std.squeeze(-1))
+            else:
+                bias_prior_mix = D.Categorical(self.mixture_weights.expand(self.out_features, -1))
+                bias_prior_comp = D.Normal(self.bias_prior_mean, self.bias_prior_std)
+                bias_prior = D.MixtureSameFamily(bias_prior_mix, bias_prior_comp)
+            result['bias'] = D.Independent(bias_prior, 1)
+        
+        return result
+
+    def _kl(self, n_samples: int) -> Tensor:
+        posterior = self._get_posterior()
+        prior = self._get_prior()
+
+        if len(self.mixture_weights) == 1:
+            kl = D.kl_divergence(posterior['weight'], prior['weight'])
+            if self.use_bias:
+                kl = kl + D.kl_divergence(posterior['bias'], prior['bias'])
+        
+        else:
+            kl_sample = []
+            for _ in range(n_samples):
+                weight = posterior['weight'].rsample()
+                kl_sample.append(posterior['weight'].log_prob(weight) - prior['weight'].log_prob(weight))
+            kl = torch.stack(kl_sample).mean()
+            if self.use_bias:
+                kl_sample = []
+                for _ in range(n_samples):
+                    bias = posterior['bias'].rsample()
+                    kl_sample.append(posterior['bias'].log_prob(bias) - prior['bias'].log_prob(bias))
+                kl_bias = torch.stack(kl_sample).mean()
+                kl += kl_bias
+
         return kl
 
     def forward(self, input: Tensor) -> Tensor:
